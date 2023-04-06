fb24ed9c2c225b149dae185482e895cd60c40ef9
hasdiannzainulfahmi <129522600+hasdiannzainulfahmi@users.noreply.github.com>
2023-04-06 22:12:07 +0800

    Add files via upload

---
+++ NILAI TERBESAR UJIAN.py
@@ -0,0 +1,6 @@
+nilai_ujian = [25, 90, 40, 95, 35, 70, 50, 20, 80, 65]
+nilai_terbesar = 0
+for nilai in nilai_ujian:
+    if nilai > nilai_terbesar:
+       nilai_terbesar = nilai
+print("nilai terbesar dari hasil ujian mahasiswa adalah:",nilai_terbesar)
---
+++ WHILE,BREAK,CONTINUE.py
@@ -0,0 +1,13 @@
+x=1
+while x < 15:
+    print (x)
+    if x ==7:
+        break
+    x +=1
+a=9
+while a < 15:
+    a +=1
+    if a ==9:
+        continue
+    print (a) 
+    
